762c2ae93fac5ce1672503b3c1918b731dfc8f4a
ofentse <ofentse.khampe@partner.bmw.co.za>
2022-07-21 09:25:15 +0200

    initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,32 @@
+import{Task} from './Task'
+
+export const TASKS:Task[]=[
+    {
+        id:1,
+        text:'Finish my stories',
+        day:'May 5th at 2:30pm',
+        reminder:true,
+    },
+   
+    {
+        
+        id:2,
+        text:'Finish my stories',
+        day:'May 5th at 2:30pm',
+        reminder:false,
+    },
+    {
+        
+        id:3,
+        text:'Finish my stories',
+        day:'May 5th at 2:30pm',
+        reminder:true,
+    },
+    {
+        
+        id:4,
+        text:'Finish my stories',
+        day:'May 5th at 2:30pm',
+        reminder:true,
+    }
+]
